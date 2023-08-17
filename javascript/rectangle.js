577fcb837130aaff98468dee4ebfd3a6598146f7
BGal23 <131159103+BGal23@users.noreply.github.com>
2023-08-17 15:10:16 +0200

    good result

--- javascript/rectangle.js
+++ javascript/rectangle.js
@@ -1,10 +1,15 @@
 "use strict";
 let length;
 let width;
+let lengthSelect;
+let widthSelect;
+let resultSelect;
 
 const circuit = document.getElementById("circuit");
 const area = document.getElementById("area");
 const diagonal = document.getElementById("diagonal");
+const ratio = document.getElementById("ratio")
+
 const lengthAlert = document.querySelector(".length-alert");
 const widthAlert = document.querySelector(".width-alert");
 
@@ -14,6 +19,9 @@ buttonResult.addEventListener("click", () => {
     function rectangle(length, width) {
     length = document.getElementById("length").value;
     width = document.getElementById("width").value;
+    lengthSelect = document.getElementById("length-select").value;
+    widthSelect = document.getElementById("width-select").value;
+    resultSelect = document.getElementById("result-select").value;
         
         if (length.length === 0 || length == 0) {
             lengthAlert.textContent = " Wpisz długość!";
@@ -33,9 +41,59 @@ buttonResult.addEventListener("click", () => {
         else {
             widthAlert.textContent = "";
         }
-        circuit.textContent = 2 * length + 2 * width;
-        area.textContent = length * width;
-        diagonal.textContent = Math.sqrt(Math.pow(length, 2) + Math.pow(width, 2))
+        
+        switch (lengthSelect) {
+            case "mm":
+                length = length * 0.1;
+                break;
+            case "cm":
+                length;
+                break;
+            case "m":
+                length = length * 100;
+                break;
+            case "km":
+                length = length * 1000;
+                break;
+        };
+                switch (widthSelect) {
+            case "mm":
+                width = width * 0.1;
+                break;
+            case "cm":
+                width;
+                break;
+            case "m":
+                width = width * 100;
+                break;
+            case "km":
+                width = width * 1000;
+                break;
+        };
+        switch (resultSelect) {
+            case "mm":
+                length = length * 10;
+                width = width * 10;
+                break;
+            case "cm":
+                length;
+                width;
+                break;
+            case "m":
+                length = length * 0.01;
+                width = width * 0.01;
+                break;
+            case "km":
+                length = length * 0.001;
+                width = width * 0.001;
+                break;
+        };
+        area.textContent =`${length * width} ${resultSelect}`;
+        circuit.textContent = `${2 * length + 2 * width} ${resultSelect}`;
+        diagonal.textContent = `${Math.sqrt(Math.pow(length, 2) + Math.pow(width, 2))} ${resultSelect}`
+        ratio.textContent = `${length / length} do ${width / length}`
+        
+
     };
     rectangle()
 });
